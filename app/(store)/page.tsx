e45df26eb9d69edc0b1c414add635ef6fa06f1d3
Oyedelehabeeb <hoyedeley4@gmail.com>
2025-02-09 15:53:41 +0100

    Add initial project setup with Sanity and Next.js integration

---
+++ app/(store)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Mr.Habee</h1>
+      <Button>Click me, I am a button</Button>
+    </div>
+  );
+}
